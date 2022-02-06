c4a5d1a352e454feb53f4ccb561bfd9e9a90ee6e
Ajit Singh <ajitsinghkaler0@gmail.com>
2022-02-06 14:09:47 +0530

    make allowance a different contract for better understanding

--- Shared wallet.sol
+++ Shared wallet.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract SharedWallet is Ownable {
+contract Allowance is Ownable{
     mapping(address => uint) public allowanceMapping;
 
     function addAllowanceUser(address _user, uint _value) public onlyOwner {
@@ -18,6 +18,10 @@ contract SharedWallet is Ownable {
     function reduceAllowance(address _allowanceUser, uint _allowanceUsed) internal{
         allowanceMapping[_allowanceUser] -= _allowanceUsed;
     }
+}
+
+contract SharedWallet is Allowance {
+    
 
     function withdraw(address payable _to, uint _value) public allownaceOrOwner(_value) {
         require(_value <= address(this).balance, "There are not enough funds in the contract");
